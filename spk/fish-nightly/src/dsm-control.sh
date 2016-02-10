e77739a60b873067cf10f09ea5e54498cd0369e2
tmaone <mds@monads.io>
2016-02-10 01:12:41 +0100

    adding fish-nightly (builds artifact using most recent git version)

---
+++ spk/fish-nightly/src/dsm-control.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Package
+PACKAGE="fish-nightly"
+DNAME="fish-nightly"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+
+case $1 in
+    start)
+        exit 0
+        ;;
+    stop)
+        exit 0
+        ;;
+    status)
+        exit 0
+        ;;
+    log)
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
+esac
